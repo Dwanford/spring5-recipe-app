314dbea64ddec753ef814975bf20122c40a00a5d
dwanford <dwanford@hotmail.com>
2022-10-05 17:31:16 +0300

    adding recipe bootstrap with 2 recipes, recipe service with implementation, list of recipes in index page

--- src/main/java/dwanford/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/dwanford/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,32 +1,24 @@
 package dwanford.springframework.spring5recipeapp.controllers;
 
-import dwanford.springframework.spring5recipeapp.domain.Category;
-import dwanford.springframework.spring5recipeapp.domain.UnitOfMeasure;
-import dwanford.springframework.spring5recipeapp.repositories.CategoryRepository;
-import dwanford.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import dwanford.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "index.html"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+    public String getIndexPage(Model model){
 
-        System.out.println("Category ID is: " + categoryOptional.get().getId());
-        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
---
+++ src/main/java/dwanford/springframework/spring5recipeapp/services/RecipeService.java
@@ -0,0 +1,11 @@
+package dwanford.springframework.spring5recipeapp.services;
+
+import dwanford.springframework.spring5recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+}
