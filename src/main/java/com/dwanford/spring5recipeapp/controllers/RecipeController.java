a2dab200b8c896ff1dfc08a62e1fbed72eeb9428
dwanford <dwanford@hotmail.com>
2022-10-14 22:19:44 +0300

    added recipe form, add new recipe mapping in recipe controller

--- src/main/java/com/dwanford/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/dwanford/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.dwanford.spring5recipeapp.controllers;
 
+import com.dwanford.spring5recipeapp.commands.RecipeCommand;
 import com.dwanford.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,10 +20,24 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show/")
     public String getRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show/";
     }
 
 }
